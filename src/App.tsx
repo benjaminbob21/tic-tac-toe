56d1a269f93501dc96dd4735f8750895558f2771
Benjamin Bamisile <bambobtim@gmail.com>
2024-05-24 02:20:51 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import Board from "./pages/Board"
+import "./global.css"
+
+function App() {
+  return (
+    <Board/>
+  )
+}
+
+export default App
---
+++ src/components/Square.tsx
@@ -0,0 +1,106 @@
+// type Player = "X" | "O" | null;
+
+// type Props = {
+//   winner: Player;
+//   value: Player;
+//   onClick: () => void;
+// };
+
+// const Square = ({ winner, value, onClick }: Props) => {
+//   if (!value) {
+//     return <button className="square w-100 h-100 text-5xl bg-white border border-solid border-black border-1" onClick={onClick} disabled={Boolean(winner)} />;
+//   }
+//   return (
+//     <button
+//   className={
+//     value === "O"
+//       ? `square w-100 h-100 text-5xl border border-solid border-black border-1 bg-green`
+//       : `square w-100 h-100 text-5xl border border-solid border-black border-1 bg-crimson`
+//   }
+//     >
+//       This is a Square
+//     </button>
+//   );
+// };
+
+// export default Square;
+
+// type Player = "X" | "O" | "BOTH" | null;
+
+// function Square({
+//   value,
+//   onClick,
+//   winner,
+// }: {
+//   winner: Player;
+//   value: Player;
+//   onClick: () => void;
+// }) {
+//   if (!value) {
+//     return (
+//       <button
+//         className="square w-100 h-100 text-5xl bg-white border border-solid border-black border-1"
+//         onClick={onClick}
+//         disabled={Boolean(winner)}
+//       />
+//     );
+//   }
+//   return (
+//     <button
+//       className={
+//         value === "O"
+//           ? `square w-100 h-100 text-5xl border border-solid border-black border-1 bg-green`
+//           : `square w-100 h-100 text-5xl border border-solid border-black border-1 bg-crimson`
+//       }
+//       disabled
+//     >
+//       {value}
+//     </button>
+//   );
+// }
+
+// export default Square;
+
+type Player = "X" | "O" | "BOTH" | null;
+
+function Square({
+  value,
+  onClick,
+  winner,
+}: {
+  winner: Player;
+  value: Player;
+  onClick: () => void;
+}) {
+  if (!value) {
+    return (
+      <button
+        className="w-[100px] h-[100px] text-[5rem] text-[#e8e5e5] border-[solid] border-[1px] border-[black] flex justify-center items-center"
+        onClick={onClick}
+        disabled={Boolean(winner)}
+      />
+    );
+  }
+  if (value === "O") {
+    return (
+      <button
+        className="w-[100px] h-[100px] text-[5rem] text-[#e8e5e5] border-solid border-1 border-black bg-green-500 flex justify-center items-center"
+        disabled
+      >
+        {value}
+      </button>
+    );
+  }
+  if (value === "X") {
+    return (
+      <button
+        className="bg-red-500 w-[100px] h-[100px] text-[5rem] text-[#e8e5e5] border border-solid border-1 border-black flex justify-center items-center"
+        disabled
+      >
+        {value}
+      </button>
+    );
+  }
+}
+
+export default Square;
